a301ecd0a23104d45e6cf205dc74b3595a86a392
Luiz Filipe Alves Coelho de Oliveira <spdauditoria@gmail.com>
2025-10-18 18:41:20 +0200

    Add swagger documentation for node

--- backend-node/src/app.ts
+++ backend-node/src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import YAML from 'yaml';
+import swaggerUi from 'swagger-ui-express';
 import ideasRouter from './routes/ideas';
 
 dotenv.config();
@@ -8,6 +12,14 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// serve swagger UI if swagger.yaml exists
+const swaggerPath = path.join(__dirname, '..', 'swagger.yaml');
+if (fs.existsSync(swaggerPath)) {
+  const swaggerFile = fs.readFileSync(swaggerPath, 'utf8');
+  const swaggerDoc = YAML.parse(swaggerFile);
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+}
 
 app.use('/ideas', ideasRouter);
 app.get('/users', async (req, res) => {
