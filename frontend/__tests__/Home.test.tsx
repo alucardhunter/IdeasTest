d5046d24a6eb085a81c93ac1fc8bba6bc920a708
Luiz Filipe Alves Coelho de Oliveira <spdauditoria@gmail.com>
2025-10-19 21:15:56 +0200

    Fix frontend test

--- frontend/__tests__/Home.test.tsx
+++ frontend/__tests__/Home.test.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { vi, test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Home from '../src/pages/Home'
-import * as api from '../src/api/api'
-import { BrowserRouter } from 'react-router-dom'
-import React from 'react'
+import { UserProvider } from '../src/context/UserContext'
 
 vi.mock('../src/api/api', () => ({
   getIdeas: vi.fn().mockResolvedValue([]),
@@ -10,6 +11,6 @@ vi.mock('../src/api/api', () => ({
 }))
 
 test('Home renders', async () => {
-  render(<BrowserRouter><Home /></BrowserRouter>)
-  expect(await screen.findByText('Ideias')).toBeTruthy()
+  render(<BrowserRouter><UserProvider><Home /></UserProvider></BrowserRouter>)
+  expect(await screen.findByLabelText('add')).toBeTruthy()
 })
